2bb8dccaa89b6961ae9103dc145b92f1d8e91dae
vava <thomasniets@gmail.com>
2025-02-04 13:23:31 +0100

    type

--- app/main.c
+++ app/main.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+static void type_builtin(char *input)
+{
+  if (!strcmp(&input[5], "echo") || !strcmp(&input[5], "exit") || !strcmp(&input[5], "type"))
+    printf("%s is a shell builtin\n", &input[5]);
+  else
+    printf("%s: command not found\n", &input[5]);
+}
+
 int main() {
   char input[100];
   while (1)
@@ -16,6 +24,8 @@ int main() {
         exit(0);
       else if (strncmp(input, "echo ", 5) == 0)
         printf("%s\n", &input[5]);
+      else if (strncmp(input, "type ", 5) == 0)
+        type_builtin(input);
       else
         printf("%s: command not found\n", input);
     }
