0969cb27dff1daa07cc688accf5918aa5a70d9ce
vava <thomasniets@gmail.com>
2025-02-04 12:54:17 +0100

    infinite prompt

--- app/main.c
+++ app/main.c
@@ -2,19 +2,22 @@
 #include <string.h>
 
 int main() {
-  // Flush after every printf
-  setbuf(stdout, NULL);
-
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  if (input[0] != '\0')
+  while (1)
   {
-    input[strlen(input) - 1] = '\0';
-    printf("%s: command not found\n", input);
+    // Flush after every printf
+    setbuf(stdout, NULL);
+
+    // Uncomment this block to pass the first stage
+    printf("$ ");
+
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin); //fgets results in \n in the end
+    if (input[0] != '\n')
+    {
+      input[strlen(input) - 1] = '\0';
+      printf("%s: command not found\n", input);
+    }
   }
   return 0;
 }
