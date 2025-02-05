3dfedbc68aac229354e7271e968eec29a14e1c72
vava <thomasniets@gmail.com>
2025-02-05 11:51:46 +0100

    execvp and fork

--- app/main.c
+++ app/main.c
@@ -2,17 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <signal.h>
+
+extern char **environ;
 
 static char *find_exec(char *path, char *cmd)
 {
 	char *res = malloc(1024);
 	char *string = strdup(path);
-	// printf("path: %s\n", string);
 	char *token = strtok(string, ":");
 	while (token != NULL)
 	{
 		snprintf(res, 1024, "%s/%s", token, cmd);
-		// printf("checking: %s\n", res);
 		if (access(res, X_OK) == 0)
 		{
 			free(string);
@@ -20,8 +22,94 @@ static char *find_exec(char *path, char *cmd)
 		}
 		token = strtok(NULL, ":");
 	}
+	free(res);
 	free(string);
 	return (NULL);
+}
+
+// static int check_cmd(char *input, char *path)
+// {
+// 	char *cmd;
+// 	pid_t	pid;
+// 	int i = 0;
+// 	while (input[i] && !isspace(input[i]))
+// 		i++;
+// 	int len = i + 1;
+// 	cmd = malloc(len);
+// 	strlcpy(cmd, input, len);
+// 	printf("cmd: %s\n", cmd);
+// 	char *res = find_exec(path, cmd);
+// 	if (!res)
+// 	{
+// 		free(cmd);
+// 		return(1);
+// 	}
+// 	else
+// 	{
+// 		printf("res: %s\n", res);
+// 		printf("input: %s\n", input);
+// 		pid = fork();
+// 		if (pid < 0)
+// 		{
+// 			free(cmd);
+// 			free(res);
+// 			perror("Fork error");
+// 			return(1);
+// 		}
+// 		if (pid == 0)
+// 		{
+// 			printf("i'm child\n");
+// 			execve(res, input, environ);
+// 		}
+// 		else
+// 		{
+// 			waitpid(pid, NULL, 0);
+// 			printf("child is done\n");
+// 			free(cmd);
+// 			free(res);
+// 			return(0);
+// 		}
+// 	}
+// 	return(1);
+// }
+
+static int check_cmd(char *input)
+{
+    pid_t pid;
+    char *cmd;
+    char *argv[20];
+    char *token = strtok(input, " ");
+    int i = 0;
+    while (token && i < 19)
+    {
+        argv[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    argv[i] = NULL;
+    if (!argv[0])
+        return (1);
+    // printf("cmd: %s\n", argv[0]);
+    pid = fork();
+    if (pid < 0)
+    {
+        perror("Fork error");
+        return (1);
+    }
+    if (pid == 0)
+    {
+        if (execvp(argv[0], argv) == -1)
+			return (1);
+        // perror("execvp failed");
+        // exit(1);
+    }
+    else
+    {
+        waitpid(pid, NULL, 0);
+        // printf("child is done\n");
+		kill(pid, 3);
+        return (0);
+    }
+	return(1);
 }
 
 static void type_builtin(char *input, char *path)
@@ -40,6 +128,11 @@ int main()
 {
 	char input[100];
 	char *path = getenv("PATH");
+	if (!path)
+	{
+		fprintf(stderr, "Error: PATH not set\n");
+		exit(1);
+	}
 	while (1)
 	{
 		printf("$ ");
@@ -54,8 +147,10 @@ int main()
 				printf("%s\n", &input[5]);
 			else if (strncmp(input, "type ", 5) == 0)
 				type_builtin(input, path);
-			else
+			// else if (check_cmd(input, path) == 1)
+			else if (check_cmd(input) == 1)
 				printf("%s: command not found\n", input);
+			// else
 		}
 	}
 	return (0);
