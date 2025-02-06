0d244d2e6125c8abb996a72ec7344a0f61d5c40d
vava <thomasniets@gmail.com>
2025-02-06 18:52:03 +0100

    formated

--- app/main.c
+++ app/main.c
@@ -25,43 +25,43 @@ static char *find_exec(char *path, char *cmd)
 
 static int fork_execvp(char *input)
 {
-    pid_t pid;
-    char *cmd;
+	pid_t pid;
+	char *cmd;
 	char *args[MAX_TOKENS];
 	if (tokenize(input, args) == -1)
-		return(1);
-    pid = fork();
-    if (pid < 0)
-    {
-        perror("Fork error");
-        return (1);
-    }
-    if (pid == 0)
-    {
-        if (execvp(args[0], args) == -1)
+		return (1);
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("Fork error");
+		return (1);
+	}
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
 			_exit(1);
-    }
-    else
-    {
+	}
+	else
+	{
 		int status;
-        waitpid(pid, &status, 0);
+		waitpid(pid, &status, 0);
 		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-            return (1);
-        return (0);
-    }
-	return(1);
+			return (1);
+		return (0);
+	}
+	return (1);
 }
 
 static void echo_builtin(char *input)
 {
 	char *args[MAX_TOKENS];
-	int i = 2; //skip echo itself and first space
+	int i = 2; // skip echo itself and first space
 	int args_num = tokenize_for_echo(input, args);
 	if (args_num == -1)
-		return ;
+		return;
 	while (i < args_num - 1)
 	{
-		printf("%s", args[i]); //spaces saved as arguments
+		printf("%s", args[i]); // spaces saved as arguments
 		i++;
 	}
 	printf("%s\n", args[i]); //-n option not handled
@@ -76,7 +76,7 @@ static void type_builtin(char *input, char *path)
 		if (!strcmp(cmd, builtins[i]))
 		{
 			printf("%s is a shell builtin\n", cmd);
-			return ;
+			return;
 		}
 		i++;
 	}
@@ -126,7 +126,7 @@ int main()
 			input[strlen(input) - 1] = '\0';
 			if (!strcmp(input, "exit 0"))
 				exit(0);
-			else if (!strncmp(input, "echo ", 5)) //needs tokenisation
+			else if (!strncmp(input, "echo ", 5)) // needs tokenisation
 				echo_builtin(input);
 			else if (!strcmp(input, "pwd"))
 				printf("%s\n", getcwd(NULL, 1));
@@ -134,7 +134,7 @@ int main()
 				change_dir(&input[3]);
 			else if (!strncmp(input, "type ", 5))
 				type_builtin(input, path);
-			else if (fork_execvp(input) == 1) //needs tokenisation
+			else if (fork_execvp(input) == 1) // needs tokenisation
 				printf("%s: command not found\n", input);
 		}
 	}
--- app/tokenize.c
+++ app/tokenize.c
@@ -2,108 +2,108 @@
 
 static char *skip_whitespace(char *str)
 {
-    while (*str && isspace(*str))
-        str++;
-    return (str);
+	while (*str && isspace(*str))
+		str++;
+	return (str);
 }
 
-static char *extract_quoted(char *str, char quote_type) {
-    char *current = str + 1;
-    while (*current != quote_type && *current != '\0')
-        current++;
-    if (*current == quote_type)
-        return (current + 1);
-    return (NULL);
+static char *extract_quoted(char *str, char quote_type)
+{
+	char *current = str + 1;
+	while (*current != quote_type && *current != '\0')
+		current++;
+	if (*current == quote_type)
+		return (current + 1);
+	return (NULL);
 }
 
 static char *extract_unquoted(char *str)
 {
-    char *current = str;
-    while (*current != '\0' && !isspace(*current) && *current != '\'' && *current != '"')
-        current++;
-    return (current);
+	char *current = str;
+	while (*current != '\0' && !isspace(*current) && *current != '\'' && *current != '"')
+		current++;
+	return (current);
 }
 
 int tokenize_for_echo(char *input, char **args)
 {
-	//probably better to make a copy of input before modifying it directly
+	// probably better to make a copy of input before modifying it directly
 	char *token_start;
 	char quote_type;
-    int token_num = 0;
-    char *pos = input;
-    char *new_pos;
+	int token_num = 0;
+	char *pos = input;
+	char *new_pos;
 	pos = skip_whitespace(pos);
-    while (*pos != '\0')
+	while (*pos != '\0')
 	{
-        new_pos = skip_whitespace(pos);
+		new_pos = skip_whitespace(pos);
 		if (new_pos != pos)
 		{
 			pos = new_pos;
 			args[token_num] = strdup(" ");
 			token_num++;
 		}
-        if (*pos == '\0')
-            break ;
-        token_start = pos;
-        if (*pos == '\'' || *pos == '"')
+		if (*pos == '\0')
+			break;
+		token_start = pos;
+		if (*pos == '\'' || *pos == '"')
 		{
-            quote_type = *pos;
-            pos = extract_quoted(pos, quote_type);
-            token_start++;
+			quote_type = *pos;
+			pos = extract_quoted(pos, quote_type);
+			token_start++;
 			args[token_num] = strndup(token_start, pos - token_start - 1);
-        }
+		}
 		else
 		{
-            pos = extract_unquoted(pos);
+			pos = extract_unquoted(pos);
 			args[token_num] = strndup(token_start, pos - token_start);
 		}
-		//will also save empty '' or "" token (important for echo)
-        if (!args[token_num])
+		// will also save empty '' or "" token (important for echo)
+		if (!args[token_num])
 		{
 			printf("strndup error\n");
-            return (-1);
+			return (-1);
 		}
-        token_num++;
-    }
-    args[token_num] = NULL;
-    return (token_num);
+		token_num++;
+	}
+	args[token_num] = NULL;
+	return (token_num);
 }
-
 
 int tokenize(char *input, char **args)
 {
-	//probably better to make a copy of input before modifying it directly
+	// probably better to make a copy of input before modifying it directly
 	char *token_start;
 	char quote_type;
-    int token_num = 0;
-    char *pos = input;
-    char *new_pos;
-    while (*pos != '\0')
+	int token_num = 0;
+	char *pos = input;
+	char *new_pos;
+	while (*pos != '\0')
 	{
-        pos = skip_whitespace(pos);
-        if (*pos == '\0')
-            break ;
-        token_start = pos;
-        if (*pos == '\'' || *pos == '"')
+		pos = skip_whitespace(pos);
+		if (*pos == '\0')
+			break;
+		token_start = pos;
+		if (*pos == '\'' || *pos == '"')
 		{
-            quote_type = *pos;
-            pos = extract_quoted(pos, quote_type);
-            token_start++;
+			quote_type = *pos;
+			pos = extract_quoted(pos, quote_type);
+			token_start++;
 			args[token_num] = strndup(token_start, pos - token_start - 1);
-        }
+		}
 		else
 		{
-            pos = extract_unquoted(pos);
+			pos = extract_unquoted(pos);
 			args[token_num] = strndup(token_start, pos - token_start);
 		}
-		//will also save empty '' or "" token (important for echo)
-        if (!args[token_num])
+		// will also save empty '' or "" token (important for echo)
+		if (!args[token_num])
 		{
 			printf("strndup error\n");
-            return (-1);
+			return (-1);
 		}
-        token_num++;
-    }
-    args[token_num] = NULL;
-    return (token_num);
+		token_num++;
+	}
+	args[token_num] = NULL;
+	return (token_num);
 }
